de3ac997e7cbe271d0ac0ecf9b58ac6379b88916
kharne8 <cerasta81@gmail.com>
2021-12-06 20:45:53 -0600

    routes setup done

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to Conctact Keeper API...' });
 });
 
+//Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
