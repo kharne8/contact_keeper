81f7ec3db8e843449b62bb9864afdc520ef1c3b4
kharne8 <cerasta81@gmail.com>
2021-12-06 20:29:28 -0600

    Express server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.port || 5000;
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to Conctact Keeper API...' });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
