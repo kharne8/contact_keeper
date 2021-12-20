f6d1ca4292186547a6fd94f0a1d40b415c953b9d
kharne8 <cerasta81@gmail.com>
2021-12-20 12:10:48 -0600

    ready for deployment

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
